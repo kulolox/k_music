ebc4318d47342c8592ce1a94115efa534d5e9959
jianfeng.xu <1048016718@qq.com>
2020-09-09 11:08:41 +0800

    引入antd，路径别名配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import React, { useCallback } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from './store/rootReducer';
-import { increment, decrement } from './store/counterSlice';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+const Home = React.lazy(() => import('@pages/home'));
+
 function App(): JSX.Element {
-  const counter = useSelector((state: RootState) => state.counter);
-  const dispatch = useDispatch();
-
-  const incrementAction = useCallback(() => {
-    dispatch(increment());
-  }, [dispatch]);
-
-  const decrementAction = useCallback(() => {
-    dispatch(decrement());
-  }, [dispatch]);
-
-  console.log('counter:', counter);
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <button onClick={incrementAction}>increment</button>
-        <button onClick={decrementAction}>decrement</button>
-        <p>{counter}</p>
-      </header>
-    </div>
+    <Router>
+      <React.Suspense fallback='加载中...'>
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </React.Suspense>
+    </Router>
   );
 }
 
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (): JSX.Element => {
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
